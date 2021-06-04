475cf95d76e2be4fd6f52ccefbbc4a282bd2bfc2
cqxg <cqxg@yandex.ru>
2021-06-04 15:45:16 +0300

    feat: add simple service example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 
+import { AppService } from "./app.service";
+
 @Controller('/api')
 export class AppController {
+    constructor(private appService: AppService) { }
 
     @Get('/users')
     getUsers() {
-        return [{ id: 1, name: '69 squad' }]
+        return this.appService.getUsers();
     }
 };
