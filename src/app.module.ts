0404ed6c9d6059b427fdb2bc60dd67883ede0b95
cqxg <cqxg@yandex.ru>
2021-06-07 16:26:01 +0300

    feat: add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5433,
-            username: 'postgres',
-            password: 'root',
-            database: 'nest-examples',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
             autoLoadModels: true
         }),
