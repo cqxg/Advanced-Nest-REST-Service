bf0fe7a74ee558727603a686c23a0407e5821111
cqxg <cqxg@yandex.ru>
2021-06-08 11:21:42 +0300

    feat: add first swagger simple documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@yandex.ru', description: 'email' })
   readonly email: string;
+
+  @ApiProperty({ example: '1234567890', description: 'password' })
   readonly password: string;
 };
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface UserCreationAttrs {
@@ -7,18 +8,23 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Unique identificator, id' })
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number;
 
+  @ApiProperty({ example: 'user@yandex.ru', description: 'email' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({ example: '1234567890', description: 'password' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: 'true', description: 'blocked or no' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({ example: 'For indecent behavior', description: 'Reason for blocking' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
 };
