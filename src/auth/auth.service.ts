3b789ebd1cf869e80ceacde17e80c8d40799f8cd
cqxg <cqxg@yandex.ru>
2021-06-11 19:09:39 +0300

    feat: add login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
@@ -10,25 +10,36 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 export class AuthService {
 
   constructor(private userService: UsersService,
-    private jwtService: JwtService) { }
+    private jwtService: JwtService) { };
 
-  async generateToken(user: User) {
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+    if (user && passwordEquals) return user;
+    throw new UnauthorizedException({ message: 'Incorrect email or password' });
+  };
+
+  private async generateToken(user: User) {
     const payload = { email: user.email, id: user.id, roles: user.roles }
 
     return {
       token: this.jwtService.sign(payload)
     }
-  }
+  };
 
   async login(userDto: CreateUserDto) {
-  }
+    const user = await this.validateUser(userDto);
+
+    return this.generateToken(user);
+  };
 
   async registration(userDto: CreateUserDto) {
     const candidate = await this.userService.getUserByEmail(userDto.email);
+
     if (candidate) throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST);
     const hashPassword = await bcrypt.hash(userDto.password, 5);
     const user = await this.userService.createUser({ ...userDto, password: hashPassword });
 
     return this.generateToken(user)
-  }
+  };
 }
