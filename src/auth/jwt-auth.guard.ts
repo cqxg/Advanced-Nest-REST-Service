ea0ea8d50554afba2e0af8ca2b197d96d5bddd3c
cqxg <cqxg@yandex.ru>
2021-07-11 21:38:28 +0300

    fix: jwt-auth.guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,7 +1,8 @@
-import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Observable } from "rxjs";
 
+@Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) { }
 
@@ -11,7 +12,7 @@ export class JwtAuthGuard implements CanActivate {
     try {
       const authHeader = req.headers.authorization;
       const bearer = authHeader.split(' ')[0];
-      const token = authHeader.split('')[1];
+      const token = authHeader.split(' ')[1];
 
       if (bearer !== 'Bearer' || !token) throw new UnauthorizedException({ message: 'User is not logged in' });
 
