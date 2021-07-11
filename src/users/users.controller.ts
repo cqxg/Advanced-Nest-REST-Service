7445bd6befdc5af6029eda0afe2b243f95a5c664
cqxg <cqxg@yandex.ru>
2021-07-11 19:52:22 +0300

    feat: add UseGuards in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { User } from './users.model';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
@@ -18,6 +19,7 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
